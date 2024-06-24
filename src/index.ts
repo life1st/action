693cd5b223792e35e0ec0f36880686b23a2333ba
jiaoyang <jiaoyang@01.ai>
2024-06-24 14:13:25 +0800

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { BaseContext, Context } from 'koa'
 import Koa from 'koa'
+import bodyParser from '@koa/bodyparser'
 // import type {  } from '@types/koa'
 const app = new Koa()
 
-app.use((ctx: Context) => {
+app
+.use(bodyParser())
+.use((ctx: Context) => {
     console.log(ctx.request)
     ctx.body = ctx.request.query || 'hello world'
 })
