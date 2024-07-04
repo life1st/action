6c775331f488055054eda42640ef7f57e2cc8744
jiaoyang <jiaoyang@01.ai>
2024-07-04 13:50:57 +0800

    feat: get branch name

--- i18n.convert.js
+++ i18n.convert.js
@@ -1,11 +1,14 @@
-const { promises: fs } = require('fs')
-const path = require('path')
-const axios = require('axios')
+import { promises as fs } from 'fs'
+import path from 'path'
+import axios from 'axios'
 
-const entry = './src/data'
+const entry = './src/i18n/en'
 
 async function sendReq(data) {
-    const isDev = !process.argv.includes('--action')
+    const branchName = process.argv.find(arg => arg.includes('branch'))
+    const prodBranchs = ['master', 'main']
+    console.log(branchName)
+    const isDev = prodBranchs.every(name => !branchName.includes(name))
     const devHost = 'http://localhost:3196'
     const prodHost = 'https://1lzpwnnf-3196.inc1.devtunnels.ms'
     const path = '/convert'
@@ -14,7 +17,7 @@ async function sendReq(data) {
     return result
 }
 
-async function run() {
+async function getLangs() {
     const files = await fs.readdir(entry)
     const data = {}
     for (let filePath of files) {
@@ -25,7 +28,12 @@ async function run() {
             data[name] = JSON.parse(content)
         }
     }
-    const result = await sendReq(data)   
+    return data
+}
+
+async function run() {
+    const data = await getLangs()
+    const result = await sendReq(data)
     console.log(result.status, result.data)
 }
 
